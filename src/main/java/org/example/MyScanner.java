53a98d3588c37d27580f607923b0f41e000f141e
mihaigheorghe <mihai.gheorghe@devm8.com>
2023-11-07 00:08:01 +0200

    added file output  + comment documentation

--- src/main/java/org/example/MyScanner.java
+++ src/main/java/org/example/MyScanner.java
@@ -31,46 +31,61 @@ public class MyScanner {
     public void scan() throws FileNotFoundException {
         String[] fileContent = readFile().split("\n");
 
+        // go line by line
         for(int j = 0; j < fileContent.length; j++) {
             String line = fileContent[j].trim();
+            // if the line is not empty
             if(!line.isEmpty()) {
                 StringBuilder word = new StringBuilder();
                 for (int i = 0; i < line.length(); i++) {
                     char character = line.charAt(i);
+                    // check for constant strings
                     if(character == '"'){
+                        // build the word with the chars after the first '"'
                         do{
                             character = line.charAt(i);
                             word.append(character);
                             i++;
+                            // stop when you reach the second " or when you get ot the end of the line
                         } while(i < line.length() && line.charAt(i) != '"');
+                        // if the end was not reached => reached the second " => add " to the word
                         if(i < line.length()){
                             character = line.charAt(i);
                             word.append(character);
                         }
-                        System.out.println("word is: "  + word);
+
+                    // check for separators, if you reached a separator or ' ' => a word was finished
                     } else if (isSeparator(String.valueOf(character)) || character == ' ' ) {
 
+                        // handle the finished word
                         handleInternalTables(word.toString(), j);
 
+                        // is a separator was reached  add it to pif
                         if(character != ' ') {
                             if (isSeparator(String.valueOf(character))) {
                                 pifTable.add(new Pair<>(String.valueOf(separators.get(String.valueOf(character))), -1));
                             }
                         }
+
+                        // reset the word
                         word = new StringBuilder();
                     } else{
+                        // if not a separator or a " continue building the word
                         word.append(character);
                     }
                 }
+                // if the line is finished handle what is left
                 handleInternalTables(word.toString(), j);
             }
         }
 
+        // print the conclusion
         System.out.println(scanMessage());
     }
 
     private void handleInternalTables(String word, int lineNumber){
         if(!word.isEmpty()) {
+            // check every possibility and add the values to pif
             if (isReservedWord(word)) {
                 pifTable.add(new Pair<>(String.valueOf(keywords.get(word)), -1));
             } else if (isOperator(word)) {
@@ -82,6 +97,7 @@ public class MyScanner {
                 Integer id = constantSymbolTable.add(word);
                 pifTable.add(new Pair<>(word, id));
             } else {
+                // if nothing matches add an error
                 errors.add("invalid token: " + word + " at line: " + (lineNumber + 1));
             }
         }
@@ -131,6 +147,7 @@ public class MyScanner {
     }
 
     private String readFile() throws FileNotFoundException {
+        // get the file content as a string
         StringBuilder fileContent = new StringBuilder();
         Scanner scanner = new Scanner(new File(this.file));
         while(scanner.hasNextLine()){
@@ -141,6 +158,7 @@ public class MyScanner {
     }
 
     private void readTokenFile(String tokensPath) {
+        // get and store tokens from the file
         try {
             BufferedReader reader = new BufferedReader(new FileReader(tokensPath));
             String line;
@@ -168,11 +186,22 @@ public class MyScanner {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        String inputFilePath = "src/main/java/org/example/p1err.txt";
+        String inputFilePath = "src/main/java/org/example/p2.txt";
         String tokensFilePath = "src/main/java/org/example/token.in";
         MyScanner scanner = new MyScanner(inputFilePath, tokensFilePath);
         scanner.scan();
 
-        System.out.println(scanner.getPIF());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("PIF.out"))) {
+            writer.write(scanner.getPIF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ST.out"))) {
+            writer.write("const table: " + scanner.getConstantSymbolTable() + "\n");
+            writer.write("identifier table:" + scanner.getIdentifierSymbolTable());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
